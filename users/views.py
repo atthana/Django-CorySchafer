9a8605d0addaaf0d627b4aaba4349b84b946c6dd
atthana-p <atthana.p@codium.co>
2020-08-09 01:34:50 +0700

    Ep6-end, User Registration (created users app, used django-crispy-forms, load register data to database).

---
+++ users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()  # ต้องมีบรรทัดนี้นะจึง save to database
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+
